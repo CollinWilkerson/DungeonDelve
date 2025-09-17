05a7879a620bc7e717adbad8c168fd0546666a60
CollinWilkerson <156472381+CollinWilkerson@users.noreply.github.com>
2025-09-17 11:04:24 -0700

    add player walk, jump, look

--- dungeon-delve/Assets/Scripts/Tavern/PlayerController.cs
+++ dungeon-delve/Assets/Scripts/Tavern/PlayerController.cs
@@ -5,7 +5,14 @@ using UnityEngine.InputSystem;
 //A LARGE PORTION OF THIS CONTROLLER ORIGINALLY COMES FROM SPENCER GALLOLN, I TAKE NO CREDIT FOR HIS WORK: https://github.com/CollinWilkerson/SciFiFarming/blob/Multiplayer/SciFiFarming/Assets/Scripts/PlayerController.cs
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float moveLerpRate = 0.5f;
+
+    [Header("Jump")]
+    [SerializeField] private float groundedCheckDistance = 0.6f;
+    [SerializeField] private float jumpForce = 20f;
+    private bool tryJump = false;
 
     [Header("Camera Settings")]
     [SerializeField] private float mouseSensitivity = 100f;
@@ -16,6 +23,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private InputAction moveAction;
     private InputAction lookAction;
+    private InputAction jumpAction;
 
     private void Awake()
     {
@@ -23,6 +31,7 @@ public class PlayerController : MonoBehaviour
 
         moveAction = InputSystem.actions.FindAction("Move");
         lookAction = InputSystem.actions.FindAction("Look");
+        jumpAction = InputSystem.actions.FindAction("Jump");
 
         playerCamera = GetComponentInChildren<Camera>();
         if (playerCamera != null)
@@ -40,19 +49,34 @@ public class PlayerController : MonoBehaviour
     {
         HandleMovement();
         HandleLook();
+
+        if (Physics.Raycast(transform.position, Vector3.down, groundedCheckDistance) && jumpAction.IsPressed())
+        {
+            Debug.Log("tryjump");
+            tryJump = true;
+        }
     }
 
-    void HandleMovement()
+    private void FixedUpdate()
+    {
+        if (tryJump)
+        {
+            HandleJump();
+            tryJump = false;
+        }
+    }
+
+    private void HandleMovement()
     {
         float moveHorizontal = moveAction.ReadValue<Vector2>().x;
         float moveVertical = moveAction.ReadValue<Vector2>().y;
 
         Vector3 movement = transform.right * moveHorizontal + transform.forward * moveVertical;
         Vector3 targetVelocity = movement.normalized * moveSpeed;
-        rb.linearVelocity = new Vector3(targetVelocity.x, rb.linearVelocity.y, targetVelocity.z);
+        rb.linearVelocity = Vector3.Lerp( rb.linearVelocity, new Vector3(targetVelocity.x, rb.linearVelocity.y, targetVelocity.z), moveLerpRate * Time.deltaTime);
     }
 
-    void HandleLook()
+    private void HandleLook()
     {
         float mouseX = lookAction.ReadValue<Vector2>().x * mouseSensitivity * Time.deltaTime;
         float mouseY = lookAction.ReadValue<Vector2>().y * mouseSensitivity * Time.deltaTime;
@@ -65,5 +89,10 @@ public class PlayerController : MonoBehaviour
             playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         }
         transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void HandleJump()
+    {
+        rb.AddForce(Vector3.up * jumpForce);
     }
 }
