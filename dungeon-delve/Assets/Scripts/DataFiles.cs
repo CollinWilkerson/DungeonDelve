727941a9d25dd61ed5d7c249f0e69159506f6a33
CollinWilkerson <wilkerc2@my.erau.edu>
2025-10-26 14:28:25 -0700

    complete and integrate trap

--- dungeon-delve/Assets/Scripts/DataFiles.cs
+++ dungeon-delve/Assets/Scripts/DataFiles.cs
@@ -34,10 +34,10 @@ public class DataFiles : MonoBehaviour
 
     //encounters lv 3
     private static int lv3_monsterSelectionWeight = 4;
-    private static int lv3_trapSelectionWeight = 1;
+    private static int lv3_trapSelectionWeight = 2;
     private static int lv3_eventSelectionWeight = 0;
 
-    private static string[] lv3_trapSceneNames = { "Explosives" };
+    private static string[] lv3_trapSceneNames = { "Explosives", "Illusion" };
 
     private void Awake()
     {
